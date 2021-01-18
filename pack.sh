9e582b4526b8a0b011a9060d43a68db01fcdfaa7
Goh Wei Wen <goweiwen@gmail.com>
2021-01-19 07:02:47 +0800

    Minify JSON

--- pack.sh
+++ pack.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+shopt -s globstar
+for i in **/*.json; do
+	jq -c . ${i} | sponge ${i}
+done
 
 zip dist/Blanktopia.zip -r assets pack.mcmeta pack.png
 mv dist/Blanktopia.zip dist/Blanktopia-$(shasum dist/Blanktopia.zip | cut -d' ' -f1).zip
-rsync -iavzhP dist/ ubuntu@play.blanktopia.com:~/docker/caddy/files
+rsync -iavzhP dist/ root@play.blanktopia.com:~/ubuntu/docker/caddy/files
