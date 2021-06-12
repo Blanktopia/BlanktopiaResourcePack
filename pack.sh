a6830217b74867016ad838a5bc80f57dd4ec2eb5
Goh Wei Wen <goweiwen@gmail.com>
2021-06-13 06:01:40 +0800

    Unminify files and add new tables

--- pack.sh
+++ pack.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 shopt -s globstar
+
+# Minify
 for i in **/*.json; do
 	jq -c . ${i} | sponge ${i}
 done
 
 zip dist/Blanktopia.zip -r assets pack.mcmeta pack.png
 mv dist/Blanktopia.zip dist/Blanktopia-$(shasum dist/Blanktopia.zip | cut -d' ' -f1).zip
-rsync -iavzhP dist/ root@play.blanktopia.com:~/ubuntu/docker/caddy/files
+rsync -iavzhP dist/ weiwen@ssh.piggyp.ink:/opt/nomad/volumes/files/ --rsync-path="sudo rsync"
+
+# Un-minify
+for i in **/*.json; do
+	jq . ${i} | sponge ${i}
+done
